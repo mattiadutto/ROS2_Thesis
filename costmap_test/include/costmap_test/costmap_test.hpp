d7f41fa6db4aae9dbd04a3d508b7eaeef9a76b15
garabetov <78539226+garabetov@users.noreply.github.com>
2024-03-03 23:23:00 +0100

    mask of points + convex region

--- costmap_test/include/costmap_test/costmap_test.hpp
+++ costmap_test/include/costmap_test/costmap_test.hpp
@@ -28,6 +28,24 @@
 
 
 
+#include "costmap_converter_msgs/msg/obstacle_msg.hpp"
+#include "costmap_converter/costmap_converter_interface.h"
+#include "costmap_converter/costmap_to_polygons.h"
+
+#include <visualization_msgs/msg/marker_array.hpp>
+#include <visualization_msgs/msg/marker.hpp>
+
+
+#include <geometry_msgs/msg/point.hpp>
+#include <geometry_msgs/msg/point_stamped.hpp>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+
+#include "nav_msgs/msg/grid_cells.hpp"
+
+
+
+
 
 #include <cstdio>
 
@@ -38,20 +56,25 @@ public:
   GoalPose();
   ~GoalPose();
 
+  void publishAsMarker(const std::string &frame_id,const costmap_converter_msgs::msg::ObstacleArrayMsg &obstacles);
+
+
 private:
   // ROS topic publishers/subscribers
   rclcpp::Subscription<geometry_msgs::msg::PointStamped>::SharedPtr clicked_point_sub;
   rclcpp::Subscription<geometry_msgs::msg::Polygon>::SharedPtr footprint_subscriber_;
   rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr local_costmap_subscriber_;
+  rclcpp::Subscription<nav_msgs::msg::GridCells>::SharedPtr transformed_sub_;
+  rclcpp::Subscription<geometry_msgs::msg::TransformStamped>::SharedPtr tf_sub_;
 
 
   // ROS timer
   rclcpp::TimerBase::SharedPtr update_timer_;
   rclcpp::TimerBase::SharedPtr timer_;
- 
+
   // variables
   int key;
- 
+
   geometry_msgs::msg::Twist cmd_vel;
   geometry_msgs::msg::PoseStamped goal;
   geometry_msgs::msg::PoseStamped base_link_pose_;
@@ -60,7 +83,23 @@ private:
   geometry_msgs::msg::Polygon footprint_;
   
   std::shared_ptr<tf2_ros::TransformListener> tf_listener_{nullptr};
-   std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
+ // std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
+
+      std::shared_ptr<tf2_ros::Buffer> tf_buffer_;
+
+  std::unique_ptr<costmap_converter::CostmapToPolygonsDBSMCCH> costmap_converter_polygons_;
+
+  std::vector<costmap_converter::CostmapToPolygonsDBSMCCH::KeyPoint> point_vect_;
+
+  rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr marker_pub_;
+
+
+
+  std::vector<geometry_msgs::msg::Point> grid_;
+ nav_msgs::msg::GridCells transformed_grid_;
+ geometry_msgs::msg::TransformStamped received_tf_;
+
+
 
 
   rclcpp::Subscription<geometry_msgs::msg::TransformStamped>::SharedPtr tf_subscriber_;
@@ -70,6 +109,8 @@ private:
   void tfCallback(const geometry_msgs::msg::TransformStamped::SharedPtr msg) const;
   void localCostmapCallback(const nav_msgs::msg::OccupancyGrid::SharedPtr msg);
   void footprint_callback(const geometry_msgs::msg::Polygon footprint);
+  void transformed_sub_callback(const nav_msgs::msg::GridCells::SharedPtr msg);
+  void tf_sub_callback(const geometry_msgs::msg::TransformStamped::SharedPtr msg);
 
   
 };
--- costmap_test/src/costmap_test.cpp
+++ costmap_test/src/costmap_test.cpp
@@ -10,20 +10,59 @@ GoalPose::GoalPose()
 : Node("goal_pose_node")
 {
 
-auto qos = rclcpp::QoS(rclcpp::KeepLast(10));
+  auto qos = rclcpp::QoS(rclcpp::KeepLast(10));
 
-clicked_point_sub = this->create_subscription<geometry_msgs::msg::PointStamped>("scout_mini/clicked_point",100,std::bind(&GoalPose::subscriber_callback, this, _1));
+  clicked_point_sub = this->create_subscription<geometry_msgs::msg::PointStamped>("scout_mini/clicked_point",100,std::bind(&GoalPose::subscriber_callback, this, _1));
 
-local_costmap_subscriber_ = this->create_subscription<nav_msgs::msg::OccupancyGrid>(
-            "scout_mini/local_costmap/costmap", 10, std::bind(&GoalPose::localCostmapCallback, this, std::placeholders::_1));
-footprint_subscriber_ = this->create_subscription<geometry_msgs::msg::Polygon>("scout_mini/local_costmap/footprint",10,std::bind(&GoalPose::footprint_callback,this,std::placeholders::_1));
- 
-tf_buffer_ =
-      std::make_unique<tf2_ros::Buffer>(this->get_clock());
+  local_costmap_subscriber_ = this->create_subscription<nav_msgs::msg::OccupancyGrid>(
+    "scout_mini/local_costmap/costmap", 10, std::bind(&GoalPose::localCostmapCallback, this, std::placeholders::_1));
+  footprint_subscriber_ = this->create_subscription<geometry_msgs::msg::Polygon>("scout_mini/local_costmap/footprint",10,std::bind(&GoalPose::footprint_callback,this,std::placeholders::_1));
 
-tf_listener_ =
-      std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
 
+  transformed_sub_ = this->create_subscription<nav_msgs::msg::GridCells>("scout_mini/transformed_pub",10,std::bind(&GoalPose::transformed_sub_callback, this,std::placeholders::_1));
+  tf_sub_ = this->create_subscription<geometry_msgs::msg::TransformStamped>("scout_mini/tf_pub",10,std::bind(&GoalPose::tf_sub_callback, this,std::placeholders::_1));
+    tf_buffer_ = std::make_shared<tf2_ros::Buffer>(this->get_clock());
+
+        //tf_buffer_ = std::make_shared<tf2_ros::BufferClient>(this);
+  tf_listener_ =
+  std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+
+  costmap_converter_polygons_ = std::make_unique<costmap_converter::CostmapToPolygonsDBSMCCH>();
+
+
+
+/*  double minX = -1;
+  double maxX = 1;
+  double minY = -1;
+  double maxY = 1;
+  double resolution = 0.1;
+
+  point_vect_.reserve(1000);
+
+
+  for (double x = minX; x <= maxX; x += resolution)
+  {
+
+    for (double y = minY; y <= maxY; y += resolution)
+    {
+
+      costmap_converter::CostmapToPolygonsDBSMCCH::KeyPoint point;
+
+      point.x = x;
+      point.y = y;
+      point_vect_.push_back(point);
+
+    }
+  }*/
+
+
+  std::cout<<"grid created!"<<std::endl;
+
+  marker_pub_ = this->create_publisher<visualization_msgs::msg::Marker>("polygon_marker", 10);
+
+
+
+   
 }
 
 GoalPose::~GoalPose()
@@ -31,53 +70,251 @@ GoalPose::~GoalPose()
   RCLCPP_INFO(get_logger(), "goal_pose node has been terminated");
 }
 
+void GoalPose::transformed_sub_callback(const nav_msgs::msg::GridCells::SharedPtr msg)
+{
+  transformed_grid_ = *msg;
+}
+
+void GoalPose::tf_sub_callback(const geometry_msgs::msg::TransformStamped::SharedPtr msg)
+{
+  received_tf_ = *msg;
+}
+
 void GoalPose::localCostmapCallback(const nav_msgs::msg::OccupancyGrid::SharedPtr msg)
 {
 
   local_costmap = *msg; // dereference and store in private variable the occupancy grid
 
-  
-}
-void GoalPose::subscriber_callback(const geometry_msgs::msg::PointStamped clicked_point) const 
+
+   // Define the target frame (e.g., "base_link")
+  std::string target_frame = "base_link";
+
+/*geometry_msgs::msg::TransformStamped test;
+  try
+  { 
+
+   test = tf_buffer_->lookupTransform("map","base_link",tf2::TimePointZero);
+     //   transformStamped = tf_buffer->lookupTransform("base_link", "map", tf2::TimePointZero);
+
+  } catch (tf2::LookupException &ex)
+  {
+    RCLCPP_WARN(this->get_logger(), "Can't find base_link to map tf: %s", ex.what());
+
+  }*/
+
+   /*// Transform each point in the vector to the target frame
+  std::vector<costmap_converter::CostmapToPolygonsDBSMCCH::KeyPoint> transformed_points;
+  for (const auto& point : point_vect_) {
+    try {
+            // Create a point stamped in the target frame
+            geometry_msgs::msg::PointStamped point_stamped_in, point_stamped_out;
+            point_stamped_in.point.x = point.x;
+            point_stamped_in.point.y = point.y;
+            point_stamped_in.header.frame_id = "map";  // Replace "original_frame" with the frame of your points
+
+            tf_buffer_->transform(point_stamped_in, point_stamped_out, "base_link",tf2::Duration(std::chrono::seconds(5)));
+
+            // Create a new KeyPoint with transformed coordinates
+            costmap_converter::CostmapToPolygonsDBSMCCH::KeyPoint transformed_point;
+            transformed_point.x = point_stamped_out.point.x;
+            transformed_point.y = point_stamped_out.point.y;
+            transformed_points.push_back(transformed_point);
+          } catch (tf2::TransformException& ex) {
+            RCLCPP_WARN(this->get_logger(), "Failed to transform point to target frame: %s", ex.what());
+          }
+        }*/
+
+
+
+
+/// new test
+ std::vector<geometry_msgs::msg::Point> grid;
+
+point_vect_.clear();
+ 
+  double minX = received_tf_.transform.translation.x-1;
+  double maxX = received_tf_.transform.translation.x+1;
+  double minY = received_tf_.transform.translation.y-1;
+  double maxY = received_tf_.transform.translation.y+1;
+  double resolution = 0.2;
+
+
+
+  point_vect_.reserve(1000);
+
+
+  for (double x = minX; x <= maxX; x += resolution)
+  {
+
+    for (double y = minY; y <= maxY; y += resolution)
+    {
+
+      costmap_converter::CostmapToPolygonsDBSMCCH::KeyPoint point;
+
+      point.x = x;
+      point.y = y;
+      point_vect_.push_back(point);
+
+    }
+  }
+
+
+
+
+
+
+
+//////////////////////
+
+
+std::vector<costmap_converter::CostmapToPolygonsDBSMCCH::KeyPoint> transformed_points;
+
+
+
+
+for (const auto &point : transformed_grid_.cells)
 {
 
-  RCLCPP_INFO(get_logger(), "Clicked X point: %.2f",clicked_point.point.x);
-  RCLCPP_INFO(get_logger(), "Clicked Y point: %.2f",clicked_point.point.y);
+  costmap_converter::CostmapToPolygonsDBSMCCH::KeyPoint transformed_point;
+  transformed_point.x = point.x;
+  transformed_point.y = point.y;
 
-  // create instance of Costmap2D object by passing the occupancy grid of the local costmap
-  nav2_costmap_2d::Costmap2D costmap(local_costmap);
-  unsigned int mx,my;
+  transformed_points.push_back(transformed_point);
+}
+
+  /// convex hull computation
+  geometry_msgs::msg::Polygon convex_hull;
+
+  //costmap_converter_polygons_->convexHullWrapper(transformed_points, convex_hull);
+  costmap_converter_polygons_->convexHullWrapper(point_vect_, convex_hull);
+
+
+
+  costmap_converter_msgs::msg::ObstacleArrayMsg convex_hull_array;
+
+  // Create an ObstacleMsg to hold the convex hull
+  costmap_converter_msgs::msg::ObstacleMsg obstacle_msg;
+
+// Add the points of the convex hull to the ObstacleMsg
+  for (const auto& point : convex_hull.points) {
+    obstacle_msg.polygon.points.push_back(point);
+  }
+
+  convex_hull_array.obstacles.resize(1); // Make sure the vector has at least 1 element
+  convex_hull_array.obstacles[0] = obstacle_msg;
+
+
+
+  publishAsMarker("map",convex_hull_array);
+
+
+
+  // tf to base_link
+
+
+ 
+
+
 
   
-  costmap.worldToMap(clicked_point.point.x,clicked_point.point.y,mx,my);
+}
+
+void GoalPose::publishAsMarker(const std::string &frame_id,const costmap_converter_msgs::msg::ObstacleArrayMsg &obstacles)
+{
+
+   visualization_msgs::msg::Marker line_list; // creater line_list as Marker msg
+   line_list.header.frame_id = frame_id;
+   line_list.header.stamp = rclcpp::Clock().now();
+  line_list.ns = "Polygons"; // namespace of the container
+  line_list.action = visualization_msgs::msg::Marker::ADD; // add marker
+  line_list.pose.orientation.w = 1.0; 
+  line_list.id = 0;
+  line_list.type = visualization_msgs::msg::Marker::LINE_LIST; //line list type 
+
+  line_list.scale.x = 0.02;
+  line_list.color.g = 1.0;
+  line_list.color.a = 1.0;
+
+  for (const auto &obstacle : obstacles.obstacles) // iterate over each element in obstacles.obstacles over each polygon
+  {
+        //iterate over each vertex of the current polygon and create line segments by joining polygon points
+    for (int j = 0; j < (int)obstacle.polygon.points.size() - 1; ++j)
+    {
+      geometry_msgs::msg::Point line_start;
+        line_start.x = obstacle.polygon.points[j].x; // for each point j assign to line_start 
+        line_start.y = obstacle.polygon.points[j].y;
+        line_list.points.push_back(line_start); // append each vertex points to line_list.points
+        geometry_msgs::msg::Point line_end;
+        line_end.x = obstacle.polygon.points[j + 1].x; // this creates a line_end point that is j+1
+        line_end.y = obstacle.polygon.points[j + 1].y;
+        line_list.points.push_back(line_end); //every subsequent point will represent a line
+      }// close loop for current polygon
+      
+      // After iterating through all vertices of the polygon, the loop checks if the polygon is closed (i.e., if it has more than two vertices).
+      if (!obstacle.polygon.points.empty() && obstacle.polygon.points.size() != 2) // if true -> polygon is closed
+      {
+        geometry_msgs::msg::Point line_start;
+        line_start.x = obstacle.polygon.points.back().x; // add last point of polygon to line_start.x
+        line_start.y = obstacle.polygon.points.back().y;
+        line_list.points.push_back(line_start); //append these last points to line_list.points vector
+
+        if (line_list.points.size() % 2 != 0) // if number is odd its incomplete line segment
+        {
+          geometry_msgs::msg::Point line_end;
+          line_end.x = obstacle.polygon.points.front().x; // assign the first points of polygon to line_end
+          line_end.y = obstacle.polygon.points.front().y;
+          line_list.points.push_back(line_end); // append line_end to line_list.points and line segment can be constructed
+        }
+      }
+    }
+      /// NB! from line_list.points points of a line can be taken so that equation of line can be formed
+      /// NB! from line_list.points points of a vertices of the obstacle can be taken so that equation of line can be formed
+//
+    marker_pub_->publish(line_list);
+
+  }
+
+
+  void GoalPose::subscriber_callback(const geometry_msgs::msg::PointStamped clicked_point) const 
+  {
+
+    RCLCPP_INFO(get_logger(), "Clicked X point: %.2f",clicked_point.point.x);
+    RCLCPP_INFO(get_logger(), "Clicked Y point: %.2f",clicked_point.point.y);
+
+  // create instance of Costmap2D object by passing the occupancy grid of the local costmap
+    nav2_costmap_2d::Costmap2D costmap(local_costmap);
+    unsigned int mx,my;
+
+
+    costmap.worldToMap(clicked_point.point.x,clicked_point.point.y,mx,my);
 
   // get the index of the clicked cell
-  unsigned int index = costmap.getIndex(mx,my);
+    unsigned int index = costmap.getIndex(mx,my);
   // get the cost of the cell with the given index
-  unsigned char cost = costmap.getCost(index);
+    unsigned char cost = costmap.getCost(index);
 
-  RCLCPP_INFO(get_logger(), "Index: %u, Cost: %u", index, cost);
+    RCLCPP_INFO(get_logger(), "Index: %u, Cost: %u", index, cost);
 
   //costmap.saveMap("local_costmap");
 
 
-} 
+  } 
 
-void GoalPose::footprint_callback(const geometry_msgs::msg::Polygon footprint)
-{
-  footprint_ = footprint;
+  void GoalPose::footprint_callback(const geometry_msgs::msg::Polygon footprint)
+  {
+    footprint_ = footprint;
 
-}
+  }
 
 
 /*******************************************************************************
 ** Main
 *******************************************************************************/
-int main(int argc, char ** argv)
-{
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<GoalPose>());
-  rclcpp::shutdown();
+  int main(int argc, char ** argv)
+  {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<GoalPose>());
+    rclcpp::shutdown();
 
-  return 0;
-}
+    return 0;
+  }
